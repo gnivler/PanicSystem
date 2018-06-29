419609f6e605cadcd1156968fa493936bf1b13da
gnivler <gnivil@gmail.com>
2018-06-28 21:09:23 -0600

    logging and logic rework, not tested probably not working yet

---
+++ PunchinOut/Logging.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace BasicPanic
+{
+    // 'borrowed' from Morphyum
+    public class Logging
+    {
+        static string filePath = $"{Holder.ModDirectory}/Log.txt";
+        public static void LogError(Exception ex)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
+                   "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+            }
+        }
+
+        public static void LogLine(object line)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now.ToShortTimeString()} {line}");
+            }
+        }
+    }
+}
